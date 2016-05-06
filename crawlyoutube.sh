9f6f57a810dc85b51435366af4c935a05256b59e
Izzur Zuhri <izzur.zuhri@gmail.com>
2016-05-06 23:15:16 +0700

    Create folder 'res' if not available

--- crawlyoutube.sh
+++ crawlyoutube.sh
@@ -7,16 +7,19 @@ nama=`echo $f1 |  tr ' ' '-' | tr '\t' '-'`
 echo "id $nama" 
 url=${f2%?}
 if [ "$url" != "-" ]
-then
+	then
 	status=$(curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8)" -w "%{http_code}" -o temp -L --silent "$url")
 	if [[ "$status" =~ "200" ]]
-	then
+		if [[ ! -d "res" ]]; then
+			mkdir "res"
+		fi
+		then
 		now=$(date +"%d-%m-%y-%H-%M")
-		 grep "yt-lockup-title" temp | grep -Eoh 'title=".*"  a'| sed -r 's/^.{7}//' | sed -r 's/.{4}$//' > feed
-		 grep "yt-lockup-meta-info" temp | grep -Eoh "</li><li>.*</li></ul>" | sed -r 's/^.{9}//' | sed -r 's/.{10}$//' > tgl
+		grep "yt-lockup-title" temp | grep -Eoh 'title=".*"  a'| sed -r 's/^.{7}//' | sed -r 's/.{4}$//' > feed
+		grep "yt-lockup-meta-info" temp | grep -Eoh "</li><li>.*</li></ul>" | sed -r 's/^.{9}//' | sed -r 's/.{10}$//' > tgl
 		paste tgl feed | tr '[:upper:]' '[:lower:]'> res/$nama-$now-y.tsv
 	fi
 fi 
 done < $1 
-	
+
 
